1de30d2e1198c418be16548e7d2387d38f837bc7
阮光宇 <18827512636@163.com>
2019-09-12 09:15:02 +0800

    创建

--- src/main/java/com/css/declare/controller/login/LoginController.java
+++ src/main/java/com/css/declare/controller/login/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * \* User: rgy
---
+++ src/test/java/com/css/declare/LoginTest.java
@@ -0,0 +1,21 @@
+package com.css.declare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * \* User: rgy
+ * \* Date: 2019/9/10 14:39
+ * \
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginTest {
+
+    @Test
+    public void login(){
+    }
+}
